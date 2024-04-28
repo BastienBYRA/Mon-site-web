7373872434a0b262e508df8c868c9b4747160f12
Bastien <neitsabast@gmail.com>
2024-04-28 19:41:00 +0200

    build: change build process to adapt to ssl certs

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -1,12 +1,15 @@
 # Récupère le dossier du script
 dir="$(dirname "$0")"
 
-source "$dir/../.env"
+#source "$dir/../.env"
+. "$dir/../.env"
 
 # Build l'image de l'application avec NGINX
 docker run -d \
     -p 80:80 \
     -p 443:443 \
-    -v certs/:/etc/nginx/ssl \
+    -e DNS_NAME=$DNS_NAME \
+    -e WWW_DNS_NAME=$WWW_DNS_NAME \
+    -v ./certbot/conf/:/etc/nginx/ssl/:ro \
     --name $DOCKER_NAME_DEPLOY \
     $DOCKER_USERNAME/$DOCKER_IMAGE_NAME:$DOCKER_TAG
